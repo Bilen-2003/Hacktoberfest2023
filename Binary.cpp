91cfd09b8ee6c50c6991ebfcac0fdd0a1b6edd92
Bilen Antony <94629246+Bilen-2003@users.noreply.github.com>
2023-10-31 21:06:55 +0530

    Create Binary.cpp
    A simple Binary search program using c++

---
+++ Binary.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Check if the target is at the middle
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        // If the target is greater, ignore the left half
+        if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        // If the target is smaller, ignore the right half
+        else {
+            right = mid - 1;
+        }
+    }
+
+    // If the target is not in the array
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 12;
+
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result == -1) {
+        cout << "Element not found in the array." << endl;
+    } else {
+        cout << "Element found at index " << result << endl;
+    }
+
+    return 0;
+}
